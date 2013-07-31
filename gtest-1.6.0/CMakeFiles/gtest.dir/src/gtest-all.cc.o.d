98a4b50241038409edb989730d58dc3566f9b263
gnzlbg <gonzalobg88@gmail.com>
2013-07-31 10:17:46 +0200

    updated license and gitignore, removed some cmake files
    removed more auto-generated gtest files
    
    added emacs backup files to gitignore
    
    updated license again

--- gtest-1.6.0/CMakeFiles/gtest.dir/src/gtest-all.cc.o.d
+++
@@ -1,2 +0,0 @@
-gtest-1.6.0/CMakeFiles/gtest.dir/src/gtest-all.cc.o: \
-  gtest-1.6.0/src/gtest-all.cc
--- gtest-1.6.0/CMakeFiles/gtest_main.dir/src/gtest_main.cc.o.d
+++
@@ -1,2 +0,0 @@
-gtest-1.6.0/CMakeFiles/gtest_main.dir/src/gtest_main.cc.o: \
-  gtest-1.6.0/src/gtest_main.cc
